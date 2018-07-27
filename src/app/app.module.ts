b2beb759391e7557f82f3eb98c757a2fbe7802f4
M-Elsaeed <m.elsaeed1998@gmail.com>
2018-07-27 19:24:11 +0200

    home-page, product-page, shopping-page, cart-page, contact-page, about-page, navbar and footer components and imported them into app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CartPipe } from './cart.pipe';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ShoppingPageComponent } from './shopping-page/shopping-page.component';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { CartPageComponent } from './cart-page/cart-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CartPipe
+    CartPipe,
+    HomePageComponent,
+    ShoppingPageComponent,
+    ProductPageComponent,
+    CartPageComponent,
+    ContactPageComponent,
+    AboutPageComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
